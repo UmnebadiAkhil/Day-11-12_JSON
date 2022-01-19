405f49ccecc4deceee57f0e54e3b14a4a24d34ad
Akhil <akhilu5042@gmail.com>
2022-01-19 15:00:28 +0530

    JSON_Inventory_Data_Management

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InventaryMangement;
+
+Console.WriteLine("Hello, World!");
+
+FetchJsonForFile fetchJsonForRice = new FetchJsonForFile();
+
+Rice ricedata = fetchJsonForRice.ReadTheJsonFile("C:/Users/akhil/OneDrive/Desktop/TerminalCommands/RFP/InventaryMangement/New.json");
+
+Console.WriteLine(ricedata);
+
+for(int i = 0; i < ricedata.typesofRice.Count; i++)
+{
+    Console.WriteLine(" "+ricedata.typesofRice[i].name);
+    Console.WriteLine(" "+ricedata.typesofRice[i].weight);
+    Console.WriteLine(" "+ricedata.typesofRice[i].price);
+    Console.WriteLine("Total price of Rice is {0} = {1} ", ricedata.typesofRice[i].name, ricedata.typesofRice[i].weight * ricedata.typesofRice[i].price);
+}
+Console.WriteLine("\n-------------------------------\n");
+for (int i = 0; i < ricedata.typesofPulses.Count; i++)
+{
+    Console.WriteLine(" "+ricedata.typesofPulses[i].name);
+    Console.WriteLine(" "+ricedata.typesofPulses[i].weight);
+    Console.WriteLine(" "+ricedata.typesofPulses[i].price);
+    Console.WriteLine("Total price of Pulses is {0} = {1} " , ricedata.typesofPulses[i].name, ricedata.typesofPulses[i].weight* ricedata.typesofPulses[i].price);
+}
+Console.WriteLine("\n-------------------------------\n");
+
+for (int i = 0; i < ricedata.typesofWheat.Count; i++)
+{
+    Console.WriteLine(" "+ricedata.typesofWheat[i].name);
+    Console.WriteLine(" "+ricedata.typesofWheat[i].weight);
+    Console.WriteLine(" "+ricedata.typesofWheat[i].price);
+    Console.WriteLine("Total price of Wheat is {0} = {1} ", ricedata.typesofWheat[i].name, ricedata.typesofWheat[i].weight * ricedata.typesofWheat[i].price);
+}
