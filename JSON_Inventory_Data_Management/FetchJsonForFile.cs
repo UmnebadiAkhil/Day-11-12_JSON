52eded4ec370308a0697176b7d1262a6f4d9d7b8
Akhil <akhilu5042@gmail.com>
2022-01-19 15:01:32 +0530

    JSON_Inventory_Data_Management

---
+++ JSON_Inventory_Data_Management/FetchJsonForFile.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InventaryMangement
+{
+    public class FetchJsonForFile
+    {
+        public Rice ReadTheJsonFile(string Path)
+
+        {
+            using (StreamReader File = new StreamReader(Path))
+        {
+                try
+                {
+                    string json = File.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
